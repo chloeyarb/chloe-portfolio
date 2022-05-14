a7133a0fae4f6d4eaf763d3407e164fbfef67aa6
Chloe Yarborough <cmyarboro@gmail.com>
2022-05-14 10:28:21 -0400

    started the about section

---
+++ src/components/About/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function About() {
+
+    return (
+        <section>
+            <div>
+            <h1 id="about">About Me</h1>
+            </div>
+            <div>
+                <p>
+                    Originally from Maryland, I relocated to Queens, NY to pursue an undergraduate degree in economics at St. John's University. After graduating I began my career in finance. 
+                    For the last 7 years I have worked in asset management. In 2021, I decided it was time for a change and began my journey as a full stack developer and enrolled in a coding bootcamp
+                    at Rutger's University. I have gained new skills in CSS, HTML, Javascript, and the MERN stack. I enjoy both the creative and technical components in the world of coding. 
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
